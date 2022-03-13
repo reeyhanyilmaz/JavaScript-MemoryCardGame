bcfa854e3b580cbc9d9b6da910e84b348f6d4c5a
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-03-13 15:14:36 +0300

    fixed click bug

--- app.js
+++ app.js
@@ -8,121 +8,118 @@ const again = document.querySelector(".again");
 const totalTime = document.querySelector("#totalTime");
 
 let hasFlippedCard = false;
-let firstCard , secondCard;
-let lockBoard = false; //ard arda tıklamaya izin vermiyo
+let firstCard, secondCard;
+let lockBoard = false; //ard arda 4 tıklamaya izin vermemek icin
 let totalSeconds = 0;
 let interval;
 let finalTime;
 let click = -1;
 
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
 
-function flipCard(){
-    
-    if(lockBoard) return;   
-    if(this === firstCard) return;
-    this.classList.add("flip");
-    
-    if(!hasFlippedCard ){        
-        // first click        
-        hasFlippedCard =true;
-        firstCard = this; 
-        startTime();
-        return;        
-    } 
-    //second click
-    hasFlippedCard = false;  
-    secondCard =this;   
-    checkForMatch();  
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+    startTime();
+    return;
+  }
+  //second click
+  secondCard = this;
+  checkForMatch();
 }
 
-function checkForMatch(){
-    let isMatch =  firstCard.dataset.id === secondCard.dataset.id;
-    isMatch ? disableCards() : unFlipCards();
+function checkForMatch() {
+  let isMatch = firstCard.dataset.id === secondCard.dataset.id;
+  isMatch ? disableCards() : unFlipCards();
 }
 
-function disableCards(){
-//eşleştiğinde
-firstCard.removeEventListener("click", flipCard);
-secondCard.removeEventListener("click", flipCard);
-resetBoard();
-gameWon();
+function disableCards() {
+  //eşleştiğinde
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+  gameWon();
 }
 
-function unFlipCards(){
-    lockBoard= true;
-     //not match
-     setTimeout(() => {
-        firstCard.classList.remove("flip");
-        secondCard.classList.remove("flip");
-        lockBoard =false;
-    }, 700);
+function unFlipCards() {
+  lockBoard = true;
+  //not match
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 700);
 }
 
-function resetBoard(){ // card döndüğünde tekrar tıklayabilmemizi sağlar
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null]
+function resetBoard() {
+  // card döndüğünde tekrar tıklayabilmemizi sağlar
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 //refresh butonuna basildiginda
-refresh.addEventListener("click", function(){
-    confirm("Are you sure that?")  
-    location.reload();   
+refresh.addEventListener("click", function () {
+  confirm("Are you sure that?");
+  location.reload();
 });
 
+//zaman göstergesi
+function startTime() {
+  if (click === -1) {
+    interval = setInterval(function () {
+      //interval belirli araliklarla sürekli çalişmasini saglar.
+      final.innerHTML = "You won in " + finalTime + " time!";
+      finalTime = minute.innerHTML + ":" + second.innerHTML;
+      totalSeconds++;
+      second.innerHTML = pad(totalSeconds % 60);
+      minute.innerHTML = pad(parseInt(totalSeconds / 60));
+    }, 1000);
+  }
+  click = 1;
+}
 
-//zaman göstergesi 
-function startTime () {
-    if(click === -1){
-        interval = setInterval(function () { //interval belirli araliklarla sürekli çalişmasini saglar. 
-            final.innerHTML = "You won in " + finalTime + " time!"; 
-            finalTime = minute.innerHTML + ":"+ second.innerHTML;
-            totalSeconds++;
-         second.innerHTML = pad(totalSeconds % 60);
-         minute.innerHTML = pad(parseInt(totalSeconds / 60));
-        }, 1000);
-
-    }
-   click = 1; 
-};
-
-function pad(val){ // 00 şeklinde ilerlesin zaman diye yazdik burayi
-    const valString = val + "";
-    if(valString.length <2){
-        return "0" + valString;
-    } else {
-        return valString;
-    }
-};
-
+function pad(val) {
+  // 00:00 şeklinde ilerlesin zaman.
+  const valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
 
 //game won
-function gameWon(){
-        if (click < 1){ // clickler saniyenin durması için. Oyun bitse dahi saniye devam etmesini engeller.
-            firstCard = e.target;
-        }
+function gameWon() {
+  if (click < 1) {
+    // clickler saniyenin durması için. Oyun bitse dahi saniye devam etmesini engeller.
+    firstCard = e.target;
+  }
 
-        if(document.getElementsByClassName('flip').length === 12){        
-            congratsSection.classList.replace("hidden", "show");       
-            clearInterval(interval);
-            finalTime = minute.innerHTML + ":"+ second.innerHTML;
-            final.innerHTML = "You won in " + finalTime + " time!"; 
-            totalTime.innerHTML = finalTime;      
-        }
-        click = 0 ;
-};
-
+  if (document.getElementsByClassName("flip").length === 12) {
+    congratsSection.classList.replace("hidden", "show");
+    clearInterval(interval);
+    finalTime = minute.innerHTML + ":" + second.innerHTML;
+    final.innerHTML = "You won in " + finalTime + " time!";
+    totalTime.innerHTML = finalTime;
+  }
+  click = 0;
+}
 
 //congrats section'da again butonu
-again.addEventListener("click" , function () { 
-    congratsSection.classList.replace("show" , "hidden");
-    location.reload();
+again.addEventListener("click", function () {
+  congratsSection.classList.replace("show", "hidden");
+  location.reload();
 });
 
-(function shuffle(){
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random()*12)
-        card.style.order = randomPos;
-    });
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
 })();
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+cards.forEach((card) => card.addEventListener("click", flipCard));
